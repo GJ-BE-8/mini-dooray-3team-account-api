3a23b38ab8aa9a606b969ed1d78fa213b57667f4
kisuja_ <tw060811@naver.com>
2024-11-28 13:24:10 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/minidooray3team/MiniDooray3teamApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray3team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDooray3teamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDooray3teamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray3team/MiniDooray3teamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray3team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDooray3teamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
