bf9de3b503b481b789e34f27a2b460356a17ed4d
kisuja_ <tw060811@naver.com>
2024-11-28 13:54:10 +0900

    commit

--- src/main/java/com/nhnacademy/minidooray3team/dto/AccountRegisterDto.java
+++ src/main/java/com/nhnacademy/minidooray3team/dto/AccountRegisterDto.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.minidooray3team.dto;
 
+
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.nhnacademy.minidooray3team.domain.Role;
